018ec3ad6a570190055ce2e7f645ab227be8018c
Jerald Victor <Jeraldvictor.Bj@cerner.com>
2024-02-19 09:19:00 +0530

    Seeds time fix

--- src/db/seeders/20240219032234-slots-dummy-data.js
+++ src/db/seeders/20240219032234-slots-dummy-data.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const dayjs = require('dayjs')
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -9,20 +11,20 @@ module.exports = {
         // User 1
         {
           user_id: 1,
-          check_in_day: '18-02-2024',
-          check_in_time: '10:00:00',
-          check_out_day: '18-02-2024',
-          check_out_time: '10:30:00',
+          check_in_day: '2024-02-18',
+          check_in_time: dayjs(`2024-02-18 10:00:00`).toDate(),
+          check_out_day: '2024-02-18',
+          check_out_time: dayjs(`2024-02-18 10:30:00`).toDate(),
           hours: 0.5,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           user_id: 1,
-          check_in_day: '18-02-2024',
-          check_in_time: '10:31:00',
-          check_out_day: '18-02-2024',
-          check_out_time: '11:30:00',
+          check_in_day: '2024-02-18',
+          check_in_time: dayjs(`2024-02-18 10:31:00`).toDate(),
+          check_out_day: '2024-02-18',
+          check_out_time: dayjs(`2024-02-18 11:30:00`).toDate(),
           hours: 0.98,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -31,20 +33,20 @@ module.exports = {
         // User 2
         {
           user_id: 2,
-          check_in_day: '18-02-2024',
-          check_in_time: '10:00:00',
-          check_out_day: '18-02-2024',
-          check_out_time: '10:30:00',
+          check_in_day: '2024-02-18',
+          check_in_time: dayjs(`2024-02-18 10:00:00`).toDate(),
+          check_out_day: '2024-02-18',
+          check_out_time: dayjs(`2024-02-18 10:20:00`).toDate(),
           hours: 0.5,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           user_id: 2,
-          check_in_day: '19-02-2024',
-          check_in_time: '10:30:00',
-          check_out_day: '19-02-2024',
-          check_out_time: '11:30:00',
+          check_in_day: '2024-02-19',
+          check_in_time: dayjs(`2024-02-19 10:30:00`).toDate(),
+          check_out_day: '2024-02-19',
+          check_out_time: dayjs(`2024-02-19 11:30:00`).toDate(),
           hours: 1.0,
           createdAt: new Date(),
           updatedAt: new Date(),
